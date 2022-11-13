2233c0e5444aaf826e86fbcdbb5d1ead90b561bd
fatemeh-tb <ftm.talebi79@gmail.com>
2022-11-13 19:49:15 +0330

    Initial IdentityServer

---
+++ Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace identityServerSample.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
